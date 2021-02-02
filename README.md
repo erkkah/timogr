55ff6b6c399a925415acce5ceeba082ae8f6cb1c
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-02 08:21:13 +0100

    Stuff

---
+++ README.md
@@ -0,0 +1,11 @@
+# TiMoGr ?
+
+https://developer.android.com/studio#command-tools
+
+sdkmanager
+
+https://developer.okta.com/blog/2018/08/10/basic-android-without-an-ide
+
+
+https://github.com/android/ndk-samples/blob/main/native-activity/app/src/main/cpp/main.cpp
+
--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,11 +1,22 @@
 #include "/home/erkkah/src/tigr/tigr.h"
 
 extern "C" void tigrMain()  {
-    Tigr *screen = tigrWindow(320, 240, "Hello", 0);
-	while (!tigrClosed(screen) && !tigrKeyDown(screen, TK_ESCAPE))
+    Tigr *screen = tigrWindow(800, 600, "Hello", TIGR_AUTO | TIGR_4X);
+    int x;
+    int y;
+    int buttons;
+	while (!tigrClosed(screen))
 	{
+        tigrMouse(screen, &x, &y, &buttons);
 		tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
 		tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff), "Hello, world.");
+        if (buttons != 0) {
+            TPixel lineColor = tigrRGB(0, 0, 0);
+            tigrLine(screen, 0, 0, x, y, lineColor);
+            tigrLine(screen, 0, screen->h - 1, x, y, lineColor);
+            tigrLine(screen, screen->w - 1, 0, x, y, lineColor);
+            tigrLine(screen, screen->w - 1, screen->h - 1, x, y, lineColor);
+        }
 		tigrUpdate(screen);
 	}
 	tigrFree(screen);
