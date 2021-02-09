66c5cac883e932d264c21973fa6fc8eeefca14cb
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-09 21:19:53 +0100

    Update demo

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,14 +1,37 @@
 #include "/Users/erik/src/tigr/tigr.h"
+#include <stdio.h>
 
 extern "C" void tigrMain() {
-    Tigr* screen = tigrWindow(800, 600, "Hello", TIGR_AUTO | TIGR_4X);
+    Tigr* screen = tigrWindow(1, 1, "Hello", TIGR_AUTO | TIGR_4X);
     int x;
     int y;
     int buttons;
+
+    const char* message = "Hello, world.";
+    int textHeight = tigrTextHeight(tfont, message);
+    int textWidth = tigrTextWidth(tfont, message);
+
+    tigrTime();
+    float liveTime = 0;
+
     while (!tigrClosed(screen)) {
+        liveTime += tigrTime();
+        if (liveTime > 5) {
+            break;
+        }
+
         tigrMouse(screen, &x, &y, &buttons);
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
-        tigrPrint(screen, tfont, 120, 110, tigrRGB(0xff, 0xff, 0xff), "Hello, world.");
+
+        int textX = (screen->w - textWidth) / 2;
+        int textY = (screen->h - textHeight) / 2;
+
+        tigrPrint(screen, tfont, textX, textY, tigrRGB(0xff, 0xff, 0xff), message);
+
+        char buf[32];
+        sprintf(buf, "%.2f", liveTime);
+        tigrPrint(screen, tfont, textX, textY + 20, tigrRGB(0xff, 0xff, 0xff), buf);
+        
         if (buttons != 0) {
             TPixel lineColor = tigrRGB(0, 0, 0);
             tigrLine(screen, 0, 0, x, y, lineColor);
