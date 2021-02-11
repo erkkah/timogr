d812d5d2a24fa7ef298298b8e2da4edd3d39d51b
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-11 10:01:28 +0100

    Show touch handling in demo

--- app/src/main/cpp/main.c
+++ app/src/main/cpp/main.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 /**
- * TiGr entry point.
+ * TIGR entry point.
  */
 void tigrMain() {
     Tigr* screen = tigrWindow(1, 1, "Hello", TIGR_AUTO | TIGR_4X);
+
     int x;
     int y;
     int buttons;
+    TigrTouchPoint touchPoints[3];
 
     const char* message = "Hello, world.";
     int textHeight = tigrTextHeight(tfont, message);
@@ -20,7 +22,24 @@ void tigrMain() {
         liveTime += tigrTime();
 
         tigrMouse(screen, &x, &y, &buttons);
+        int numTouches = tigrTouch(screen, touchPoints, 3);
+
         tigrClear(screen, tigrRGB(0x80, 0x90, 0xa0));
+
+        TPixel mouseLineColor = tigrRGB(0, 0, 0);
+        if (buttons != 0) {
+            tigrLine(screen, 0, 0, x, y, mouseLineColor);
+            tigrLine(screen, 0, screen->h - 1, x, y, mouseLineColor);
+            tigrLine(screen, screen->w - 1, 0, x, y, mouseLineColor);
+            tigrLine(screen, screen->w - 1, screen->h - 1, x, y, mouseLineColor);
+        }
+
+        TPixel touchLineColor = tigrRGB(0, 0x80, 0x80);
+        for (int i = 0; i < numTouches; i++) {
+            TigrTouchPoint* point = touchPoints + i;
+            tigrLine(screen, 0, point->y, screen->w - 1, point->y, touchLineColor);
+            tigrLine(screen, point->x, 0, point->x, screen->h - 1, touchLineColor);
+        }
 
         int textX = (screen->w - textWidth) / 2;
         int textY = (screen->h - textHeight) / 2;
@@ -31,13 +50,6 @@ void tigrMain() {
         sprintf(buf, "%.2f", liveTime);
         tigrPrint(screen, tfont, textX, textY + 20, tigrRGB(0xff, 0xff, 0xff), buf);
         
-        if (buttons != 0) {
-            TPixel lineColor = tigrRGB(0, 0, 0);
-            tigrLine(screen, 0, 0, x, y, lineColor);
-            tigrLine(screen, 0, screen->h - 1, x, y, lineColor);
-            tigrLine(screen, screen->w - 1, 0, x, y, lineColor);
-            tigrLine(screen, screen->w - 1, screen->h - 1, x, y, lineColor);
-        }
         tigrUpdate(screen);
     }
     tigrFree(screen);
