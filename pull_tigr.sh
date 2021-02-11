e375c46ab97e48adbc39d92a7e33f0d7ed574b70
Erik Agsjö <erik.agsjo@gmail.com>
2021-02-11 16:44:06 +0100

    Use TIGR master branch

--- pull_tigr.sh
+++ pull_tigr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Pull in the latest version of TIGR
-git subtree pull --prefix tigr https://github.com/erkkah/tigr.git android --squash
+git subtree pull --prefix tigr https://github.com/erkkah/tigr.git master --squash
